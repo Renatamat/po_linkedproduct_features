a1f96a8e5feb7dcb2162e534849d283379eaeca4
Renatamat <54407224+Renatamat@users.noreply.github.com>
2026-02-05 09:26:37 +0100

    Add full PrestaShop 8.2 module structure

--- sql/features_install.php
+++ sql/features_install.php
@@ -9,7 +9,7 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_profile` (
     `family_csv` VARCHAR(255) NULL,
     `active` TINYINT(1) NOT NULL DEFAULT 1,
     PRIMARY KEY (`id_profile`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8mb4;';
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_profile_label` (
     `id_profile` INT(11) NOT NULL,
@@ -17,7 +17,7 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_profile_label` 
     `id_lang` INT(11) NOT NULL,
     `label` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id_profile`, `id_feature`, `id_lang`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8mb4;';
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_product_family` (
     `id_product` INT(11) NOT NULL,
@@ -25,7 +25,7 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_product_family`
     `family_key` VARCHAR(64) NOT NULL,
     `updated_at` DATETIME NULL,
     PRIMARY KEY (`id_product`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8mb4;';
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_index` (
     `id_product` INT(11) NOT NULL,
@@ -34,7 +34,7 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'po_link_index` (
     `options_json` TEXT NOT NULL,
     PRIMARY KEY (`id_product`),
     KEY `idx_profile_family` (`id_profile`, `family_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8mb4;';
 
 foreach ($sql as $query) {
     if (Db::getInstance()->execute($query) == false) {
