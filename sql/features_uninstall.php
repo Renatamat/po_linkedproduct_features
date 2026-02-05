d5364e8f92ace528f50c5b2e527ffbf2f24bd346
Renatamat <54407224+Renatamat@users.noreply.github.com>
2026-02-05 10:49:01 +0100

    Fix rebuild group SQL value string

--- sql/features_uninstall.php
+++ sql/features_uninstall.php
@@ -4,6 +4,7 @@ $sql = array();
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'po_link_index`';
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'po_link_product_family`';
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'po_link_profile_label`';
+$sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'po_link_group`';
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'po_link_profile`';
 
 foreach ($sql as $query) {
